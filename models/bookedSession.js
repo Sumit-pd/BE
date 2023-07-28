5784899349e885860eb122d51073926e7d8933ab
sumit <pdsumit07@gmail.com>
2023-07-28 12:54:50 +0530

    update schema

--- models/bookedSession.js
+++ models/bookedSession.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose')
 const sessionSchema = mongoose.Schema({
     student: String,
     time: String,
-    day: String
+    day: String,
+    dean: String ,
+    isBooked : Boolean
 })
 module.exports = mongoose.model('Session', sessionSchema)
