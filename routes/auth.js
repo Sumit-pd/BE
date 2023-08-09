336711be262248a185ad79b5264dce038ba59e72
sumit <pdsumit07@gmail.com>
2023-08-09 18:07:47 +0530

    completed

--- routes/auth.js
+++ routes/auth.js
@@ -17,6 +17,12 @@ router.post('/user/login', async (req, res) => {
         if (!isPasswordValid) {
             return res.status(401).json({ error: "Invalid password" })
         }
+        await User.updateOne(
+            { "universityId": universityId },
+            {
+                $set: { isLoggedIn: true }
+            }
+        )
         const data = {
             universityId, password
         }
@@ -27,5 +33,19 @@ router.post('/user/login', async (req, res) => {
         res.status(500).json({ Error: "internal server error" });
     }
 });
+
+router.post('/user/logout', async (req, res) => {
+    const { universityId } = req.body;
+    try {
+        await User.updateOne(
+            { "universityId": universityId }, { $set: { isLoggedIn: false } }
+        );
+        res.status(200).json({ message: "Logged out successfully" });
+    } catch (error) {
+        console.log("Error while logging out \n", error);
+        return res.status(500).json({ Error: "Internal server error" });
+    }
+});
+
 
 module.exports = router;
